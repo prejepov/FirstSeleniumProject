af127913180c9e27d49145cd1d6d333be724f51f
prejepov <prejepow@gmail.com>
2018-06-11 14:02:48 -0500

    selenium project

---
+++ src/com/cbt/SeleniumDemo.java
@@ -0,0 +1,17 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","/Users/parahatr/Documents/selenium dependencies/drivers/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://apple.com");
+		
+	}
+
+}
